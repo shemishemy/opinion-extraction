2d109b3e9ad4e0d81d23dd371f1264f5ea027716
shemishemy <91819485+shemishemy@users.noreply.github.com>
2022-02-26 16:22:53 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
 ]
 
 # 開発環境での設定
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
